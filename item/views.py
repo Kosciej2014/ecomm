630d5bd9cac03164f0ed9e5d63931c569fd88072
Kosciej2014 <papugth@gmail.com>
2023-02-16 20:04:39 +0100

    day_4 v0.2.5

--- item/views.py
+++ item/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from WebShop.models import Item
-
+from django.contrib.auth.decorators import login_required
+from .forms import NewItemFrom
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -9,4 +10,23 @@ def detail(request, pk):
     return render(request, 'core/detail.html', {
         'item': item,
         'related_items': related_items,
+    })
+
+@login_required()
+def new(request):
+    if request.method == 'POST':
+        form = NewItemFrom(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemFrom()
+
+    return render(request, 'core/form.html', {
+        'form': form,
+        'title': 'New item',
     })
